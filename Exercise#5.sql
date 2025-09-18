0d7d06e193d032854f379af391231200013db555
Muhammad Waqar <muhammad.9035@iqra.edu.pk>
2025-09-18 21:27:49 +0500

    Update Exercise#5.sql

--- Exercise#5.sql
+++ Exercise#5.sql
@@ -1,1 +1,8 @@
-
+-- Assuming Data from Exercise 4
+SELECT
+    CAST(e.name AS VARCHAR(100)) AS event_name,
+    COUNT(re.runner_id) AS runner_count
+FROM event e
+LEFT JOIN runner_event re ON e.id = re.event_id
+GROUP BY CAST(e.name AS VARCHAR(100)), e.id
+ORDER BY runner_count DESC, CAST(e.name AS VARCHAR(100)) ASC;
